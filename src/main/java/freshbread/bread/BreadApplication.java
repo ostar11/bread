8bafc7666c79b7e2d1d354f9a704604742283cfc
ostar11 <cjw5877@gmail.com>
2023-07-27 15:40:23 +0900

    first commit

---
+++ src/main/java/freshbread/bread/BreadApplication.java
@@ -0,0 +1,13 @@
+package freshbread.bread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/freshbread/bread/BreadApplicationTests.java
@@ -0,0 +1,13 @@
+package freshbread.bread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
