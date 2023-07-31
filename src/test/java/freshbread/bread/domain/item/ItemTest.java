40d440f0f03620dde662f6415adb957225148795
ostar11 <cjw5877@gmail.com>
2023-07-31 14:31:28 +0900

    Test: 상품 재고 증가 테스트(ItemTest)

---
+++ src/test/java/freshbread/bread/domain/item/ItemTest.java
@@ -0,0 +1,25 @@
+package freshbread.bread.domain.item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ItemTest {
+
+    @Test
+    void 재고_증가() throws Exception {
+        //given
+        Item item = createItem("식빵", 2000, 10, "우유로 만든 기본식빵");
+        //when
+        item.addStock(15);
+        //then
+        assertEquals(item.getStockQuantity(), 25);
+    }
+
+    private Item createItem(String itemName, int price, int stockQuantity, String itemDetails) {
+        return new Item(itemName, price, stockQuantity, itemDetails);
+    }
+}
