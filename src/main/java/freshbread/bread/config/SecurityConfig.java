b3d8c7837da90b9c22022e809606ee9c862b6f0d
ostar11 <cjw5877@gmail.com>
2023-08-24 15:22:25 +0900

    Refactor: spring security configuration 통합
    BCryptPasswordEncoder를 SecurityConfig 안에서 빈 생성

--- src/main/java/freshbread/bread/config/SecurityConfig.java
+++ src/main/java/freshbread/bread/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -33,8 +34,11 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/")
                 .failureUrl("/login");
 
-
-
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
