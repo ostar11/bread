a32f575acca8e1b518fff4e4f124d7f5f8e7e22a
ostar11 <cjw5877@gmail.com>
2023-08-28 16:39:18 +0900

    Feat: 상품 상세 페이지와 api 구현

--- src/main/java/freshbread/bread/controller/ItemController.java
+++ src/main/java/freshbread/bread/controller/ItemController.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +17,20 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    /**
+     * 전체 메뉴 조회
+     */
     @GetMapping
     public String menuHome(Model model) {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "item/menuList";
+    }
+
+    @GetMapping("/{itemId}")
+    public String itemView(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemService.findOne(itemId);
+        model.addAttribute(item);
+        return "item/itemDetails";
     }
 }
